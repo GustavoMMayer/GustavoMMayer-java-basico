4b7d6dc1d8930f7a82057fd741b868f7df13fc29
gustavo <gustavomiguelmayer@gmail.com>
2024-07-02 08:08:09 -0300

    Inicio de metodos

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,28 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+        int numeroDeDias = 22;
+
+
+        String concatenacao ="?"; 
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+        
+    }
+    
+}
